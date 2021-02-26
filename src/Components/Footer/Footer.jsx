948dabd2750831ba13e4fd11541e8642f6a5b98b
shubhpatel9 <shubh9atel@gmail.com>
2021-02-26 13:02:45 -0500

    footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
     return (
         <div style={data.footer.mainStyle} className="footer">
             {/* <img src={data.footer.footerLogo} alt="shubh's logo" className="foot"/> */}
-            <h2 style={data.footer.textStyle}>Designed by Shubh and created by Shubham using <a className="footerLink" rel="noreferrer" href="https://github.com/shubhpatel9/Portfolio/tree/drysol" target="_blank">React <i style={data.footer.iconStyle} className="fab fa-react"></i></a></h2>
+            <h2 style={data.footer.textStyle}>Made with <a className="footerLink" rel="noreferrer" href="https://github.com/shubhpatel9/Portfolio/tree/drysol" target="_blank">React <i style={data.footer.iconStyle} className="fab fa-react"></i></a></h2>
         </div>
     )
 }
